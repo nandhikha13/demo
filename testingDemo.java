6141e2d98ec9c1a4d9c00904b25bf9b4ee2d89fb
Nandhikha Sivakumar <nandhikha.sivakumar@gmail.com>
2024-11-26 16:41:18 -0500

    first commit
    created a functional interface

--- testingDemo.java
+++ testingDemo.java
@@ -1,7 +1,13 @@
 public class testingDemo
 {
+	@FunctionalInterface
+	public interface sumInterface
+	{
+		public int sumOfRange(int a, int b);
+	}
+
 	public static void main(String[] args)
 	{
-	
+
 	}
 }
